a6e7d9d12f83e630b76c7388f83584447c4ce860
Your Name <you@example.com>
2023-09-04 07:54:36 -0500

    users name created

--- Api_express/index.js
+++ Api_express/index.js
@@ -2,42 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Import the cors package to prevent cors error
+
 const cors = require('cors'); 
 
-// Use the cors middleware
+
 app.use(cors());
-// Use Json middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// New list for items storage (only ram memory)
+
 let items = []
 
 let users = [
   {
-    "name":"Jose",
-    last:"Moncada",
-    email:"jamoncada@icesi.edu.co"
+    name:"Isabella",
+    last:"Palma",
+    id: 100001
   },
   {
-    name:"Pepito",
-    last:"Perez",
-    email:"pperez@icesi.edu.co"
+    name:"Laura Marcela",
+    last:"Mendieta",
+    id: 100002
   },
   {
-    name:"Ana",
-    last:"Frank",
-    email:"afrank@icesi.edu.co"
+    name:"Johnny",
+    last:"Oviedo",
+    id: 100003
   },
 ]
 
-// 📤 Endpoint for obtain all items
 app.get('/items', (req, res) => {  
   res.send({"items":items})
 })
 
-// 📥 Enpoint for save one new item
 app.post('/items', (req, res) => {
   const newItem = {
     x: req.body.x,
@@ -47,10 +45,8 @@ app.post('/items', (req, res) => {
     b: req.body.b,
   }
 
-  // Add new Item to list 
   items.push(newItem);
 
-  // Send a response
   res.send({"response":`New Item at ${newItem.x}, ${newItem.y}`});
 })
 
